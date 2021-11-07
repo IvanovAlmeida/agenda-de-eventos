3101da34a5f9bae6ba24bf7cd012fcfb30ce648b
Ivanov de Almeida <ivanovdealmeida@gmail.com>
2021-11-07 20:47:24 -0300

    Base project

---
+++ src/AgendaDeEventos.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AgendaDeEventos.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/AgendaDeEventos.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AgendaDeEventos.Domain
+{
+    public class Class1
+    {
+    }
+}
